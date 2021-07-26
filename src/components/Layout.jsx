78893954e38e1d51773787e8986f5f152fa219a5
ggroce <ggrocedev@gmail.com>
2021-07-26 13:42:31 -0500

    fixing previous layout issues

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
-import Header from './Header';
-import Sidebar from './Sidebar';
-import Footer from './Footer';
+import React from 'react'
+import styled from 'styled-components'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Footer from './Footer'
 
 const SelectedPageParent = styled.div`
-  background: #B5CCB8;
+  background: #b5ccb8;
   margin-left: 175px;
   margin-top: 50px;
   flex: 1;
-`;
+`
 
 function Layout(props) {
-  return(
+  return (
     <div id="parent">
       <Header />
-      <Sidebar history={props.history}/>
+      <Sidebar history={props.history} />
 
       <main>
         <div className="scroll-wrapper">
-            {props.children}
+          {props.children}
+          <Footer />
         </div>
       </main>
-      <Footer />
-
     </div>
   )
 }
 
-export default Layout;
+export default Layout
